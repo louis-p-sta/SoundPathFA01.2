2787c6fd4f157104bed815e86ba594da07873f64
Chris <Christopher.rivard123@gmail.com>
2023-09-24 18:55:26 -0400

    Test de commentaire

--- app/src/main/java/com/example/soundpathempty/MainActivity.kt
+++ app/src/main/java/com/example/soundpathempty/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.soundpathempty.ui.theme.SoundPathEmptyTheme
-
+//Test de commit
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
