f8f64cca8a33ba08efa16de6228e84b6a2d4f4b6
Chris <Christopher.rivard123@gmail.com>
2023-10-11 13:34:52 -0400

    Changed main page so it loads

--- app/src/main/java/com/example/soundpathempty/MainActivity.kt
+++ app/src/main/java/com/example/soundpathempty/MainActivity.kt
@@ -15,32 +15,22 @@ import com.example.soundpathempty.ui.theme.SoundPathEmptyTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SoundPathEmptyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.layout)
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SoundPathEmptyTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        SoundPathEmptyTheme {
+            Greeting("Android")
+        }
     }
 }
