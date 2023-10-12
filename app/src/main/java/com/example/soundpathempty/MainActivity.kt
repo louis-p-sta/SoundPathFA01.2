530cbae0ecd9c968c7177fa9e8d5a4fea7a1f34f
Chris <Christopher.rivard123@gmail.com>
2023-10-12 12:37:56 -0400

    Button to change activity to the routes page works. The one that goes to Location doesn't work as there is something in the following page that crashes the app. Investigating

--- app/src/main/java/com/example/soundpathempty/MainActivity.kt
+++ app/src/main/java/com/example/soundpathempty/MainActivity.kt
@@ -11,26 +11,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.soundpathempty.ui.theme.SoundPathEmptyTheme
+import android.location.LocationListener
+import android.location.Location
+import android.location.LocationManager
+import android.widget.Button
+import android.widget.TextView
+import android.Manifest
+import android.content.pm.PackageManager
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+
 //Test de commit
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout)
-    }
-
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        SoundPathEmptyTheme {
-            Greeting("Android")
+        title = "SplashPage"
+        val routesbutton:Button = findViewById(R.id.routes)
+        routesbutton.setOnClickListener {
+            val intent = Intent(this@MainActivity, Routes::class.java)
+            startActivity(intent)
+        }
+        val locationbutton:Button = findViewById(R.id.location)
+        locationbutton.setOnClickListener {
+            val locationpage = Intent(this@MainActivity, WhereAmI::class.java)
+            startActivity(locationpage)
         }
     }
 }
---
+++ app/src/main/java/com/example/soundpathempty/WhereAmI.kt
@@ -0,0 +1,67 @@
+package com.example.soundpathempty
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.widget.Toast
+import android.content.Intent
+
+class WhereAmI : AppCompatActivity(), LocationListener{
+    private lateinit var locationManager: LocationManager
+    private lateinit var tvGpsLocation: TextView
+    private val locationPermissionCode = 2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.positiongps)
+        title = "LocationPage"
+        val locationbutton: Button = findViewById<Button>(R.id.getLocation)
+        locationbutton.setOnClickListener {
+            getLocation()
+        }
+        val button: Button = findViewById(R.id.menuButton)
+        button.setOnClickListener {
+            val i = Intent(this@WhereAmI, MainActivity::class.java)
+            startActivity(i)
+        }
+
+    }
+
+    private fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+    }
+
+    override fun onLocationChanged(location: Location) {
+        tvGpsLocation = findViewById(R.id.textView)
+        tvGpsLocation.text =
+            "Latitude: " + location.latitude + " , Longitude: " + location.longitude
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
