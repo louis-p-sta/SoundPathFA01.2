2eeb582106e2b6b8bf7cf516b19a4a264a219c51
Louis-Philippe St-Arnaud <99094186+louis-p-sta@users.noreply.github.com>
2023-10-18 22:41:01 -0400

    Test de code différent pour fused location provider client (vérifier avec phone de Chris).

--- app/src/main/java/com/example/soundpathempty/WhereAmI.kt
+++ app/src/main/java/com/example/soundpathempty/WhereAmI.kt
@@ -1,17 +1,17 @@
 package com.example.soundpathempty
 
-import android.content.Context
+import android.Manifest
+import android.content.Intent
+import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.widget.Button
-import android.content.Intent
-import android.location.LocationListener
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import android.Manifest
-import com.google.android.gms.common.util.VisibleForTesting
+import java.lang.String
+import kotlin.Exception
 
 class WhereAmI : ComponentActivity() {
     private lateinit var locationManager: LocationManager
@@ -23,6 +23,7 @@ class WhereAmI : ComponentActivity() {
         setContentView(R.layout.positiongps)
         title = "Position GPS"
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        //Need a location request, since location is null
         val menubutton: Button = findViewById(R.id.menuButton)
         menubutton.setOnClickListener {
             val i = Intent(this@WhereAmI, MainActivity::class.java)
@@ -31,7 +32,26 @@ class WhereAmI : ComponentActivity() {
         val gpslocation: TextView = findViewById(R.id.textView)
         val getlocation: Button = findViewById(R.id.getLocation)
         getlocation.setOnClickListener {
-            gpslocation.text = "Current location is \n" + "Lat : ${fusedLocationProviderClient.lastLocation.result.latitude}"
+            try {
+                //gpslocation.text =
+                   // "Current location is \n" + "Lat : ${fusedLocationProviderClient.lastLocation.result.latitude}"
+                //println("${fusedLocationProviderClient.getCurrentLocation(1,)}")
+                //println("${fusedLocationProviderClient.lastLocation.result.latitude}")
+                var wayLatitude : Double = 10.0
+                var wayLongitude: Double = 10.0
+                fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this) { location ->
+                    println("Location : $location")
+                    if (location != null) {
+                        wayLatitude = location.getLatitude()
+                        wayLongitude = location.getLongitude()
+                    }
+                }
+
+                println("$wayLatitude")
+                gpslocation.text = "Current location is \n" + "Lat : ${wayLatitude}"
+            }catch(e:Exception) {
+                println("Oops")
+            }
         }
 
         fun GPSPROVIDER(){
